42f3381486c3ed901f4231f6bcabf0c090168815
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-11-15 19:44:48 +0100

    wired with api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+  $.get("http://ipinfo.io", function(response) {
+    $('#hidden_div').prepend("<img src='https://landing-page-api.herokuapp.com/visited?currentApp=querply&version=1&ipAddress="+ response.ip + "' />");
+    $('.ipAddress').val(response.ip);
+  }, "jsonp");
 
+  if (window.location.search.indexOf('signedup') != -1) {
+    $('#thankyou').modal();
+  }
 });
